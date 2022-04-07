dc01a37afad5e898181f4f30872b5fd39dd519ad
Mike Haid <haidmichaelj@gmail.com>
2022-04-06 20:41:10 -0500

    completed Part2 JST

--- api/index.js
+++ api/index.js
@@ -1,5 +1,16 @@
 const express = require('express');
 const apiRouter = express.Router();
+const jwt = require('jsonwebtoken');
+
+const token = jwt.sign({ id: 3, username: 'joshua'}, 'server secret', { expiresIn: '1h' });
+
+token;
+
+const recoveredData = jwt.verify(token, 'server secret');
+
+recoveredData;
+
+jwt.verify(token, 'server secret');
 
 const usersRouter = require('./users');
 apiRouter.use('/users', usersRouter);
